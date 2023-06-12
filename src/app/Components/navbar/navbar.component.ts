89cc652e74d6a13dd4186b7edf41a014b632136e
Harshit Laxkar <hlaxkar@gmail.com>
2023-06-12 20:56:49 +0530

    fixing navbar

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  navlinks = [
+    {id:1, title: 'Library', icon: 'Collection.svg' },
+    {id:2, title: 'History', icon: 'History.svg' },
+    {id:3,title: 'Updates', icon: 'NewRelease.svg' },
+    {id:4, title: 'Browse', icon: 'Explore.svg' },
+    {id:5, title: 'More', icon: 'meatball.svg' }
+  ];
 }
