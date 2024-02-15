bdb0db1e827506ab430833f8811feeda63fd7a73
hlaxkar <hlaxkar@gmail.com>
2024-02-15 19:10:03 +0530

    updates

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require("electron");
+const url = require("url");
+const path = require("path");
+
+let win;
+
 function createWindow() {
-  win = new BrowserWindow({width: 800, height: 800});
-  win.loadFile('dist/Tachiyomi-web/index.html');
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadURL("http://localhost:4200"); // Use the URL of your Angular development server
+
+  win.on("closed", () => {
+    win = null;
+  });
 }
 
-app.whenReady().then(() => {
-  createWindow()
-})
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
